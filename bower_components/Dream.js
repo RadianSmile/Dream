9b12a89f31cc8c3c94af9b724c360f45fb804355
Radian <craziestdavid@gmail.com>
2015-12-28 01:42:01 +0800

    First Page

---
+++ bower_components/Dream.js
@@ -0,0 +1,19 @@
+
+(function(root){
+
+	if (!root.Snap ){
+		console.error("Rn.Warn : Module Snap is not defined. Please check the library is included before Dream.js included.");
+		return "" ;
+	}
+
+	var Dream = {} ;
+
+	Dream.Bg = {} ;
+
+	Dream.Bg.fillTriangle = function (svgSelector){
+		var s = Snap (svgId) ;
+	} ;
+
+
+	return Dream ;
+})(window);
